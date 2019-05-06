82db0eeeab02ba2c77bbaf825796213a733baf29
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2019-05-06 02:55:45 -0400

    Add machinery to deal with allocators

--- include/fs/allocation.hpp
+++ include/fs/allocation.hpp
@@ -1,2 +1,14 @@
 #pragma once
 
+#include "fs/api.hpp"
+#include <EASTL/polyalloc/polyalloc.h>
+
+namespace fs {
+#ifdef LIBFS_DISABLE_GLOBAL_ALLOCATOR
+#	define LIBFS_GET_GLOBAL_ALLOC
+#else
+#	define LIBFS_GET_GLOBAL_ALLOC = get_global_allocator()
+#endif
+	LIBFS_EXPORT void set_global_allocator(const eastl::polyalloc::allocator_handle& handle);
+	LIBFS_EXPORT eastl::polyalloc::allocator_handle get_global_allocator();
+} // namespace fs
---
+++ src/allocation.cpp
@@ -0,0 +1,40 @@
+#include "fs/allocation.hpp"
+#include <mutex>
+
+namespace fs {
+	class mallocator final : public eastl::polyalloc::allocator {
+	  public:
+		mallocator() = default;
+		mallocator(mallocator const&) = delete;
+		mallocator(mallocator&&) = delete;
+		mallocator& operator=(mallocator const&) = delete;
+		mallocator& operator=(mallocator&&) = delete;
+
+		~mallocator() override = default;
+
+		void* allocate(size_t n, int flags = 0) override {
+			return malloc(n);
+		}
+		void* allocate(size_t n, size_t alignment, size_t offset, int flags = 0) override {
+			return aligned_alloc(n, alignment);
+		}
+		void deallocate(void* p, size_t n) override {
+			free(p);
+		}
+	};
+
+	static std::mutex global_alloc_guard;
+	static mallocator global_alloc_mallocator;
+	static eastl::polyalloc::allocator_handle global_alloc_handle = &global_alloc_mallocator;
+
+	void set_global_allocator(eastl::polyalloc::allocator_handle const& handle) {
+		std::lock_guard lg(global_alloc_guard);
+		global_alloc_handle = handle;
+	}
+
+	eastl::polyalloc::allocator_handle get_global_allocator() {
+		std::lock_guard lg(global_alloc_guard);
+		return global_alloc_handle;
+	}
+
+} // namespace fs
